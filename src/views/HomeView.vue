a727de624a8fa7d6784a31114f1f1fe0f0e38187
Lami <Halam.Lee.Lami@gmail.com>
2023-09-07 15:52:31 +0900

    feat: Create Route With Params

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,8 @@
           <li
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
-            class="py-2 cursor-pointer">
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -30,6 +31,22 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(',');
+  router.push({
+    name: 'cityView',
+    params: { state: state.replaceAll(' ', ''), city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey =
   'pk.eyJ1IjoibGFtaTEyMyIsImEiOiJjbG04NTRqbDMwNjZ4M3FyMGdmMjkzazlzIn0.sEEfFbovboYuBN08JsFilQ';
